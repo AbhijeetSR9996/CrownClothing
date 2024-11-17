6c87c3c6e85711d251a4701f3c718f81f6f64bcd
Abhijeet Singh Rathour <47415323+AbhijeetSR9996@users.noreply.github.com>
2024-11-17 19:47:34 +0530

    Update checkout-item.component.js

--- src/components/checkout-item/checkout-item.component.js
+++ src/components/checkout-item/checkout-item.component.js
@@ -22,7 +22,7 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='name'> {name} </span>
       <span className='quantity'>
         <div className='arrow' onClick={removeItemHandler}>
-          // &#10094;➖
+           {/* &#10094; */}➖
         </div>
         <span className='value'>{quantity}</span>
         <div className='arrow' onClick={addItemHandler}>
@@ -31,7 +31,7 @@ const CheckoutItem = ({ cartItem }) => {
       </span>
       <span className='price'> {price}</span>
       <div className='remove-button' onClick={clearItemHandler}>
-        {/* &#10005; */} ✖️
+        {/* &#10005; */} ❌
       </div>
     </div>
   );
