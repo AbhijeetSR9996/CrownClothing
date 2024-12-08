05d14ab4d4a3bae44b3b059b46f55eb61cca50c0
Abhijeet Singh Rathour <47415323+AbhijeetSR9996@users.noreply.github.com>
2024-12-08 18:40:23 +0530

    Update shop.component.js

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,80 +1,138 @@
-import {  useState } from 'react';
-import ProductCard from '../../components/product-card/product-card.component';
-//import { ProductsContext } from '../../contexts/products.context';
-import PRODUCTS from '../../shop-data.json';
-import './shop.styles.scss';
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ProductCard from "../../components/product-card/product-card.component";
+import PRODUCTS from "../../shop-data.json";
+import "./shop.styles.scss";
 import Button from "../../components/button/button.component";
 
 const Shop = () => {
-  //const { products } = useContext(ProductsContext);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const filter = params.get("filter");
+
+  useEffect(() => {
+    if (filter) {
+      applyFilter(filter);
+    }
+  }, [filter]);
+
+  const applyFilter = (filter) => {
+    console.log(`Filter applied: ${filter}`);
+    document.getElementById(filter)?.click();
+  };
 
   const [filteredProducts, setFilteredProducts] = useState(PRODUCTS);
   //Track active button
-  const [activeButton,setActiveButton]= useState("all");
+  const [activeButton, setActiveButton] = useState("all");
 
   //Filter by Caps
   const filterCaps = () => {
-    const caps = PRODUCTS.filter((item)=>item.category === "Cap");
+    const caps = PRODUCTS.filter((item) => item.category === "Cap");
     setFilteredProducts(caps);
-    setActiveButton("caps");  //set active button
-  }
+    setActiveButton("caps"); //set active button
+  };
 
   //Filter by Caps
   const filterJackets = () => {
-    const jackets = PRODUCTS.filter((item)=>item.category === "Jacket");
+    const jackets = PRODUCTS.filter((item) => item.category === "Jacket");
     setFilteredProducts(jackets);
-    setActiveButton("jackets");  //set active button
-  }
+    setActiveButton("jackets"); //set active button
+  };
 
   //Filter by Sneakers
   const filterSneakers = () => {
-    const sneakers = PRODUCTS.filter((item)=>item.category === "Sneaker");
+    const sneakers = PRODUCTS.filter((item) => item.category === "Sneaker");
     setFilteredProducts(sneakers);
-    setActiveButton("sneakers");  //set active button
-  }
-
-  //Filter by Men
-  const filterMen = () => {
-    const men = PRODUCTS.filter((item)=>item.gender === "men");
-    setFilteredProducts(men);
-    setActiveButton("men");  //set active button
-  }
+    setActiveButton("sneakers"); //set active button
+  };
 
   //Filter by Women
   const filterWomen = () => {
-    const women = PRODUCTS.filter((item)=>item.gender === "women");
+    const women = PRODUCTS.filter((item) => item.gender === "women");
     setFilteredProducts(women);
-    setActiveButton("women");  //set active button
-  }
+    setActiveButton("women"); //set active button
+  };
+
+  //Filter by Men
+  const filterMen = () => {
+    const men = PRODUCTS.filter((item) => item.gender === "men");
+    setFilteredProducts(men);
+    setActiveButton("men"); //set active button
+  };
 
   //Show All Items
   const showAll = () => {
     setFilteredProducts(PRODUCTS);
-    setActiveButton("all");  //set active button
-  }
+    setActiveButton("all"); //set active button
+  };
 
   return (
-  <div style={{flex:1}}>
-    {/* Buttons */}
-   <div style={{display:'flex',gap:'5px',justifyContent:'center'}} >
-   <Button onClick={showAll} style={{fontFamily:'Open Sans Condensed'}} buttonType={activeButton==='all'?'':'inverted'}>ALL</Button>
-   <Button onClick={filterCaps} style={{fontFamily:'Open Sans Condensed'}} buttonType={activeButton==='caps'?'':'inverted'}> CAPS </Button>
-   <Button onClick={filterJackets} style={{fontFamily:'Open Sans Condensed'}} buttonType={activeButton==='jackets'?'':'inverted'}> JACKETS </Button>
-   <Button onClick={filterSneakers} style={{fontFamily:'Open Sans Condensed'}} buttonType={activeButton==='sneakers'?'':'inverted'}> SNEAKERS </Button>
-   <Button onClick={filterMen} style={{fontFamily:'Open Sans Condensed'}} buttonType={activeButton==='men'?'':'inverted'}> MENS </Button>
-   <Button onClick={filterWomen} style={{fontFamily:'Open Sans Condensed'}} buttonType={activeButton==='women'?'':'inverted'}> WOMENS </Button>
+    <div style={{ flex: 1 }}>
+      {/* Buttons */}
+      <div style={{ display: "flex", gap: "5px", justifyContent: "center" }}>
+        <Button
+          id="all"
+          onClick={showAll}
+          style={{ fontFamily: "Open Sans Condensed" }}
+          buttonType={activeButton === "all" ? "" : "inverted"}
+        >
+          ALL
+        </Button>
+        <Button
+          id="hats"
+          onClick={filterCaps}
+          style={{ fontFamily: "Open Sans Condensed" }}
+          buttonType={activeButton === "caps" ? "" : "inverted"}
+        >
+          {" "}
+          HATS{" "}
+        </Button>
+        <Button
+          id="jackets"
+          onClick={filterJackets}
+          style={{ fontFamily: "Open Sans Condensed" }}
+          buttonType={activeButton === "jackets" ? "" : "inverted"}
+        >
+          {" "}
+          JACKETS{" "}
+        </Button>
+        <Button
+          id="sneakers"
+          onClick={filterSneakers}
+          style={{ fontFamily: "Open Sans Condensed" }}
+          buttonType={activeButton === "sneakers" ? "" : "inverted"}
+        >
+          {" "}
+          SNEAKERS{" "}
+        </Button>
+        <Button
+          id="womens"
+          onClick={filterWomen}
+          style={{ fontFamily: "Open Sans Condensed" }}
+          buttonType={activeButton === "women" ? "" : "inverted"}
+        >
+          {" "}
+          WOMENS{" "}
+        </Button>
+        <Button
+          id="mens"
+          onClick={filterMen}
+          style={{ fontFamily: "Open Sans Condensed" }}
+          buttonType={activeButton === "men" ? "" : "inverted"}
+        >
+          {" "}
+          MENS{" "}
+        </Button>
+      </div>
+      &nbsp;
+      {/* Product List */}
+      <div className="products-container">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
-    &nbsp;
-    {/* Product List */}
-    <div className='products-container'>
-      {filteredProducts.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
-   </div>
-    
   );
 };
 
 export default Shop;
-
