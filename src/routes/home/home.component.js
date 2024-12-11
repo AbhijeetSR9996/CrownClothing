bcd6ba0faa069af3d0121d724a88595822326bfb
Abhijeet Singh Rathour <47415323+AbhijeetSR9996@users.noreply.github.com>
2024-12-11 13:28:48 +0530

    Update home.component.js

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -1,61 +1,86 @@
-import { Outlet } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import Directory from "../../components/directory/directory.component";
-import Button from "../../components/button/button.component";
+import React, { useState } from "react";
+import ProductCard from "../../components/product-card/product-card.component";
+import PRODUCTS from "../../shop-data.json";
+import "./shop.styles.scss";
+//import Button from "../../components/button/button.component";
 
-const Home = () => {
-  const navigate = useNavigate();
+const Shop = () => {
+  //states
+  const [filteredProducts, setFilteredProducts] = useState(PRODUCTS);
+  const [activeButton, setActiveButton] = useState("all");
 
-  const goToShop = () => {
-    navigate("/shop");
+  //filter function
+  // const filterProducts = (category) => {
+  //   const filtered =
+  //     category === "all"
+  //       ? PRODUCTS
+  //       : PRODUCTS.filter(
+  //           (item) => item.category === category || item.gender === category
+  //         );
+  //   console.log("filtered products:", filtered);
+  //   setFilteredProducts(filtered);
+  //   setActiveButton(category);
+  // };
+
+  const filterProducts = (category) => {
+    if (category === "all") {
+      setFilteredProducts(PRODUCTS);
+    }
+    if (category === "men") {
+      setFilteredProducts(PRODUCTS.filter((item) => item.gender === category));
+    }
+    if (category === "women") {
+      setFilteredProducts(PRODUCTS.filter((item) => item.gender === category));
+    }
+    // if (category === "cap" || category === "jacket" || category === "sneaker") {
+    //   setFilteredProducts(
+    //     PRODUCTS.filter((item) => item.category === category)
+    //   );
+    // }
+    setActiveButton(category);
   };
+
+  //categories
   const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
+    { label: "ALL", category: "all" },
+    { label: "CAPS", category: "cap" },
+    { label: "JACKETS", category: "jacket" },
+    { label: "SNEAKERS", category: "sneaker" },
+    { label: "MEN", category: "men" },
+    { label: "WOMEN", category: "women" },
   ];
 
   return (
-    <>
-      <div>
-        <Directory categories={categories} />
-        <Outlet />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            backgroundColor: "transparent",
-          }}
-        >
-          <Button onClick={goToShop}>
-            STILL THINKING?? GO AND EXPLORE 👉👉 HERE 👈👈
-          </Button>
-        </div>
+    <div className="shop">
+      {/* filter buttons */}
+      <div className="filter-buttons">
+        {categories.map(({ label, category }) => (
+          <button
+            key={category}
+            onClick={() => filterProducts(category)}
+            style={{
+              fontFamily: "Open Sans Condensed",
+              backgroundColor: activeButton === category ? "#000" : "#fff",
+              color: activeButton === category ? "#fff" : "#000",
+            }}
+            buttonType={activeButton === category ? "inverted" : ""}
+          >
+            {label}
+          </button>
+        ))}
       </div>
-    </>
+      {/* Product List */}
+      <div className="products-container">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        ) : (
+          <div>No Products Found</div>
+        )}
+      </div>
+    </div>
   );
 };
 
-export default Home;
+export default Shop;
