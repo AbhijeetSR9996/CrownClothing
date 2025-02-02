2358135703ab9461194486397f30bd313fb9056b
Abhijeet Singh Rathour <47415323+AbhijeetSR9996@users.noreply.github.com>
2025-02-02 22:58:52 +0530

    Update checkout.component.js

--- src/routes/checkout/checkout.component.js
+++ src/routes/checkout/checkout.component.js
@@ -10,7 +10,7 @@ import Button from "../../components/button/button.component";
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  const { cartItems, cartTotal } = useContext(CartContext);
+  const { cartItems, cartTotal, clearCart } = useContext(CartContext); // Add clearCart from context
   const [isOpenNow, setIsOpenNow] = useState(false);
   const [isOpenLater, setIsOpenLater] = useState(false);
 
@@ -26,6 +26,7 @@ const Checkout = () => {
 
   const handleCloseNow = () => {
     setIsOpenNow(false);
+    clearCart(); // Clear the cart
     navigate("/");
   };
 
@@ -35,87 +36,87 @@ const Checkout = () => {
 
   const handleCloseLater = () => {
     setIsOpenLater(false);
+    clearCart(); // Clear the cart
     navigate("/");
   };
 
   return (
     <div className="checkout-container">
-
       {cartTotal ? (
         <>
-       <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
-        </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Price per item</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
-      </div>
-      {cartItems.map((cartItem) => (
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-        <div className="total">
-          TOTAL AMOUNT: &#x20B9;{cartTotal}
-          
-          <Button onClick={handleOpenNow}> BUY NOW </Button>
-          <div>&nbsp;</div>
-          {isOpenNow && (
-            <div
-              style={{
-                position: "fixed",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%,-50%)",
-                padding: "50px",
-                backgroundColor: "white",
-                border: "3px solid black",
-                zIndex: 1000,
-              }}
-            >
-              <h4>Payment Successful..!!</h4>
-              <center>
-                <button onClick={handleCloseNow}>TAKE ME TO HOMEPAGE</button>
-              </center>
+          <div className="checkout-header">
+            <div className="header-block">
+              <span>Product</span>
             </div>
-          )}
-          <Button buttonType="inverted" onClick={handleOpenLater}>
-            {" "}
-            BUY LATER{" "}
-          </Button>
-          {isOpenLater && (
-            <div
-              style={{
-                position: "fixed",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%,-50%)",
-                padding: "50px",
-                backgroundColor: "white",
-                border: "3px solid black",
-                zIndex: 1000,
-              }}
-            >
-              <h4>Items are moved to Saved Items List.</h4>
-              <center>
-                <button onClick={handleCloseLater}>TAKE ME TO HOMEPAGE</button>
-              </center>
+            <div className="header-block">
+              <span>Description</span>
             </div>
-          )}
-        </div>
+            <div className="header-block">
+              <span>Quantity</span>
+            </div>
+            <div className="header-block">
+              <span>Price per item</span>
+            </div>
+            <div className="header-block">
+              <span>Remove</span>
+            </div>
+          </div>
+          {cartItems.map((cartItem) => (
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+          ))}
+          <div className="total">
+            TOTAL AMOUNT: &#x20B9;{cartTotal}
+            <Button onClick={handleOpenNow}>BUY NOW</Button>
+            <div>&nbsp;</div>
+            {isOpenNow && (
+              <div
+                style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%,-50%)",
+                  padding: "50px",
+                  backgroundColor: "white",
+                  border: "3px solid black",
+                  zIndex: 1000,
+                }}
+              >
+                <h4>Payment Successful..!!</h4>
+                <center>
+                  <button onClick={handleCloseNow}>TAKE ME TO HOMEPAGE</button>
+                </center>
+              </div>
+            )}
+            <Button buttonType="inverted" onClick={handleOpenLater}>
+              BUY LATER
+            </Button>
+            {isOpenLater && (
+              <div
+                style={{
+                  position: "fixed",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%,-50%)",
+                  padding: "50px",
+                  backgroundColor: "white",
+                  border: "3px solid black",
+                  zIndex: 1000,
+                }}
+              >
+                <h4>Items are moved to Saved Items List.</h4>
+                <center>
+                  <button onClick={handleCloseLater}>
+                    TAKE ME TO HOMEPAGE
+                  </button>
+                </center>
+              </div>
+            )}
+          </div>
         </>
       ) : (
         <div>
           &nbsp;
-          <Button onClick={goToShop}> YOUR CART IS EMPTY!! SHOP NOW </Button>
+          <Button onClick={goToShop}>YOUR CART IS EMPTY!! SHOP NOW</Button>
         </div>
       )}
     </div>
